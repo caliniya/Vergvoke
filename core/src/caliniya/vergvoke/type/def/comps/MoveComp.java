ea172198adcb033288463c4f9cb1cc2e0658343e
yiyi <caliniya@outlook.com>
2026-09-19 17:46:46 +0800

    updata

--- core/src/caliniya/vergvoke/type/def/comps/MoveComp.java
+++ core/src/caliniya/vergvoke/type/def/comps/MoveComp.java
@@ -5,4 +5,18 @@ import caliniya.vergvoke.annotation.Annotations.*;
 @Component(name = "Move", index = 2, proc = "main")
 public class MoveComp {
 
+    public float speed = 5f;
+
+    public float speedt = 5f;
+
+    @Import
+    public float x;
+
+    @Import
+    public float y;
+
+    @Updata
+    public void update() {
+    }
+
 }
---
+++ core/src/caliniya/vergvoke/type/def/comps/PosComp.java
@@ -0,0 +1,11 @@
+package caliniya.vergvoke.type.def.comps;
+
+import caliniya.vergvoke.annotation.Annotations.*;
+
+@Component(name = "Pos", index = 4, proc = "main")
+public class PosComp {
+
+    public float x = 0f;
+    public float y = 0f;
+
+}
